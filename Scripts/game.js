e1a2e75e441197dec0d8408a2f05a81af6c80595
u04 <39099229+u04@users.noreply.github.com>
2020-02-14 20:12:39 -0500

    added button, image

--- Scripts/game.js
+++ Scripts/game.js
@@ -3,6 +3,7 @@
     let canvas = document.getElementById("canvas");
     let stage;
     let hiLabel;
+    let meButton;
     function Start() {
         console.log("game started");
         stage = new createjs.Stage(canvas);
@@ -15,7 +16,19 @@
     }
     function Main() {
         hiLabel = new createjs.Text("hi", "20px Consolas", "#000000");
+        hiLabel.x = 320;
+        hiLabel.y = 240;
         stage.addChild(hiLabel);
+        meButton = new createjs.Bitmap('./Assets/images/me.png');
+        //meButton.regX = meButton.getBounds().width * 0.5;
+        //meButton.regY = meButton.getBounds().height * 0.5;
+        meButton.x = 450;
+        meButton.y = 200;
+        stage.addChild(meButton);
+        meButton.on("click", function () {
+            //hiLabel.text = "touched";
+            //console.log("button pushed");
+        });
     }
     window.addEventListener("load", Start);
 })();
--- Scripts/game.ts
+++ Scripts/game.ts
@@ -3,6 +3,7 @@
     let canvas = document.getElementById("canvas");
     let stage:createjs.Stage;
     let hiLabel: createjs.Text;
+    let meButton: createjs.Bitmap;
 
     function Start():void
     {
@@ -20,7 +21,21 @@
     function Main():void
     {
         hiLabel = new createjs.Text("hi", "20px Consolas", "#000000");
+        hiLabel.x = 320;
+        hiLabel.y = 240;
         stage.addChild(hiLabel);
+
+        meButton = new createjs.Bitmap('./Assets/images/me.png');
+        //meButton.regX = meButton.getBounds().width * 0.5;
+        //meButton.regY = meButton.getBounds().height * 0.5;
+        meButton.x = 450;
+        meButton.y = 200;
+        stage.addChild(meButton);
+        
+        meButton.on("click", function(){
+            //hiLabel.text = "touched";
+            //console.log("button pushed");
+        });
     }
     window.addEventListener("load", Start);
 })();
